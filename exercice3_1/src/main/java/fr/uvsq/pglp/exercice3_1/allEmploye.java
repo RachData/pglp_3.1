94584e7fb8ae1c7df5ea59814cf088f9ef6761c6
RachData <rachidderra@gmail.com>
2020-02-26 22:15:19 +0100

    Creation de la classe employe1 et employe 2

--- exercice3_1/src/main/java/fr/uvsq/pglp/exercice3_1/allEmploye.java
+++ exercice3_1/src/main/java/fr/uvsq/pglp/exercice3_1/allEmploye.java
@@ -1,7 +1,10 @@
 package fr.uvsq.pglp.exercice3_1;
 
 public abstract class allEmploye {
+	protected String name;
+	protected String adresse;
 	
-	public abstract double salaire();
+	public abstract double calculSalaire();
+	public abstract void affichecoordonnees();
 
 }
---
+++ exercice3_1/src/main/java/fr/uvsq/pglp/exercice3_1/employe2.java
@@ -0,0 +1,24 @@
+package fr.uvsq.pglp.exercice3_1;
+
+public class employe2 extends allEmploye{
+	
+	public employe2(String name, String addresse) {
+		// TODO Auto-generated constructor stub
+		super.name=name;
+		super.adresse=adresse;
+	}
+	
+	@Override
+	public double calculSalaire() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+	@Override
+	public void affichecoordonnees() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
